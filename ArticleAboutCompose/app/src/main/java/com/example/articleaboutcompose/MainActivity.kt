f25ff323c0465a5063fdefb288fc0c94c0666dfe
marcelakramer <101991467+marcelakramer@users.noreply.github.com>
2024-07-16 23:58:33 -0300

    fix: add / before '

--- ArticleAboutCompose/app/src/main/java/com/example/articleaboutcompose/MainActivity.kt
+++ ArticleAboutCompose/app/src/main/java/com/example/articleaboutcompose/MainActivity.kt
@@ -12,7 +12,6 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
